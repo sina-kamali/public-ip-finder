fd2edaad475508942f7915bbb9159f48f25581c4
Sina <sina.kamali15@gmail.com>
2023-03-06 18:06:58 -0500

    Bash script to find Public IP and save in a file

---
+++ ip-finder.sh
@@ -0,0 +1,20 @@
+###########################################
+## Check if we have a public IP
+###########################################
+ipv4_regex='([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])\.([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])\.([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])\.([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])'
+ip=$(curl -s -4 https://cloudflare.com/cdn-cgi/trace | grep -E '^ip'); ret=$?
+if [[ ! $ret == 0 ]]; then # In the case that cloudflare failed to return an ip.
+    # Attempt to get the ip from other websites.
+    ip=$(curl -s https://api.ipify.org || curl -s https://ipv4.icanhazip.com)
+else
+    # Extract just the ip from the ip line from cloudflare.
+    ip=$(echo $ip | sed -E "s/^ip=($ipv4_regex)$/\1/")
+fi
+
+# Use regex to check for proper IPv4 format.
+if [[ ! $ip =~ ^$ipv4_regex$ ]]; then
+    logger -s "Public ip finder: Failed to find a valid IP."
+    exit 2
+fi
+
+echo $ip > /home/public-ip-finder/server-public-ip.ips
